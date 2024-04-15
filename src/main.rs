c1553f18b028cbb7da5326dca45b431ddd63721b
Nina Hammer <nina.hammer@mni.thm.de>
2024-04-15 17:08:41 +0200

    in Farbe und bunt

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
-use std::{collections::VecDeque, io::{self, BufRead}};
+use std::{
+    collections::VecDeque,
+    fmt::Display,
+    io::{self, stdout, Error, Write},
+};
+
+use colored::Colorize;
 
 fn main() {
     let mut stack: Vec<StackElement> = Vec::new();
-    println!("Welcome to Consize \n");
+    println!("{}", "Welcome to Consize\n".yellow().bold());
     let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let _line = match line {
-            Ok(line) => {
-                let mut input = line.split_whitespace().map(|s| s.to_string()).collect::<VecDeque<String>>();
-                let instructions = &mut build_stack(&mut input);
-                instructions.iter().for_each(|i| call_stack(&mut stack, i));
-            },
-            Err(_) => panic!("Failed to read line"),
-            
-        };
-        println!("{:?}", stack);
+    loop {
+        print!("{}", "=> ".bright_cyan());
+        stdout().flush().unwrap_or_else(|_| {
+            panic!("{} an error occured flushing stdout", "Error:".red().bold())
+        });
+        let mut inp = String::new();
+        stdin.read_line(&mut inp).unwrap_or_else(|_| {
+            panic!(
+                "{} something went wrong reading from stdin",
+                "Error: ".red().bold()
+            )
+        });
+
+        let mut input = inp
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<VecDeque<String>>();
+        let instructions = &mut parse(&mut input);
+        instructions
+            .iter()
+            .for_each(|i| call(&mut stack, i).unwrap_or_else(|e| println!("{e}")));
+
+        println!("{}\n", print_stack(&stack, false));
     }
 }
 
@@ -22,22 +40,46 @@ fn main() {
 enum StackElement {
     Integer(usize),
     Keyword(String),
-    SubStack(Vec<StackElement>)
+    SubStack(Vec<StackElement>),
+    Word(String),
 }
 
-fn build_stack(input: &mut VecDeque<String>) -> Vec<StackElement>{
+impl Display for StackElement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            StackElement::Integer(i) => write!(f, "{} ", i),
+            StackElement::Keyword(s) => write!(f, "{} ", s),
+            StackElement::SubStack(st) => write!(f, "{} ", print_stack(st, true)),
+            StackElement::Word(s) => write!(f, "{} ", s),
+        }
+    }
+}
+
+fn print_stack(stack: &Vec<StackElement>, print_brackets: bool) -> String {
+    let mut str = "".to_string();
+    if print_brackets {
+        str.push('[');
+    }
+    for i in stack {
+        str.push_str(format!("{}", *i).as_str())
+    }
+    if print_brackets {
+        str.push(']');
+    }
+
+    str
+}
+
+fn parse(input: &mut VecDeque<String>) -> Vec<StackElement> {
     let mut stack = Vec::new();
     while !input.is_empty() {
         let ins = input.pop_front().unwrap();
-        match ins.parse::<usize>() {
-            Ok(n) => {
-                stack.push(StackElement::Integer(n));
-                continue;
-            },
-            Err(_) => {}
+        if let Ok(n) = ins.parse::<usize>() {
+            stack.push(StackElement::Integer(n));
+            continue;
         }
-        if ins == "[" {
-            let s = build_stack(input);
+        if ins == "[ " {
+            let s = parse(input);
             stack.push(StackElement::SubStack(s));
             continue;
         }
@@ -50,93 +92,187 @@ fn build_stack(input: &mut VecDeque<String>) -> Vec<StackElement>{
     stack
 }
 
-fn call_stack(stack: &mut Vec<StackElement>, input: &StackElement) {
+fn call(stack: &mut Vec<StackElement>, input: &StackElement) -> Result<(), std::io::Error> {
     match input {
-        StackElement::Keyword(k) => {
-            match k.as_str() {
-                "+" => {
-                    match stack.pop().unwrap() {
-                        StackElement::Integer(x) => {
-                            match stack.pop().unwrap() {
-                                StackElement::Integer(y) => stack.push(StackElement::Integer(x + y)),
-                                _ => panic!("I can only add integers")
-                                
-                            }
-                        }
-                        _ => panic!("I can only add integers")
-                    }
-                },
-                "-" => {
-                    match stack.pop().unwrap() {
-                        StackElement::Integer(x) => {
-                            match stack.pop().unwrap() {
-                                StackElement::Integer(y) => stack.push(StackElement::Integer(x - y)),
-                                _ => panic!("I can only subtract integers")
-                                
-                            }
-                        }
-                        _ => panic!("I can only subtract integers")
-                    }
-                },
-                "*" => {
-                    match stack.pop().unwrap() {
-                        StackElement::Integer(x) => {
-                            match stack.pop().unwrap() {
-                                StackElement::Integer(y) => stack.push(StackElement::Integer(x * y)),
-                                _ => panic!("I can only multiply integers")
-                                
-                            }
-                        }
-                        _ => panic!("I can only multiply integers")
-                    }
-                },
-                "/" => {
-                    match stack.pop().unwrap() {
-                        StackElement::Integer(x) => {
-                            match stack.pop().unwrap() {
-                                StackElement::Integer(y) => stack.push(StackElement::Integer(x / y)),
-                                _ => panic!("I can only divide integers")
-                                
-                            }
-                        }
-                        _ => panic!("I can only divide integers")
-                    }
-                },
-                "clear" => stack.clear(),
-                "dup" => {
-                    let a = stack.pop().unwrap();
-                    stack.push(a.clone());
-                    stack.push(a);
-                },
-                "swap" => {
-                    let a = stack.pop().unwrap();
-                    let b = stack.pop().unwrap();
-                    stack.push(a.clone());
-                    stack.push(b.clone());
-                },
-                "drop" => {
-                    stack.pop().unwrap();
-                },
-                "rot" => {
-                    let a = stack.pop().unwrap();
-                    let b = stack.pop().unwrap();
-                    let c = stack.pop().unwrap();
-
-                    stack.push(b);
-                    stack.push(a);
-                    stack.push(c);
+        StackElement::Keyword(k) => match k.as_str() {
+            "+" => {
+                if stack.len() < 2 {
+                    return Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} not enough operands", "Error:".red().bold()),
+                    ));
                 }
-                "call" => {
-                    match stack.pop().unwrap() {
-                        StackElement::SubStack(s) => s.iter().for_each(|i| call_stack(stack, i)),
-                        _ => {}
-                    }
+                match stack.pop().unwrap() {
+                    StackElement::Integer(x) => match stack.pop().unwrap() {
+                        StackElement::Integer(y) => {
+                            stack.push(StackElement::Integer(x + y));
+                            Ok(())
+                        }
+                        _ => Err(Error::new(
+                            io::ErrorKind::InvalidInput,
+                            format!("{} I can only add Integers", "Error:".red().bold()),
+                        )),
+                    },
+                    _ => Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} I can only add Integers", "Error:".red().bold()),
+                    )),
                 }
-                _ => panic!("Invalid input"),
             }
+            "-" => {
+                if stack.len() < 2 {
+                    return Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} not enough operands", "Error:".red().bold()),
+                    ));
+                }
+                match stack.pop().unwrap() {
+                    StackElement::Integer(x) => match stack.pop().unwrap() {
+                        StackElement::Integer(y) => {
+                            stack.push(StackElement::Integer(x - y));
+                            Ok(())
+                        }
+                        _ => Err(Error::new(
+                            io::ErrorKind::InvalidInput,
+                            format!("{} I can only subtract Integers", "Error:".red().bold()),
+                        )),
+                    },
+                    _ => Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} I can only subtract Integers", "Error:".red().bold()),
+                    )),
+                }
+            }
+            "*" => {
+                if stack.len() < 2 {
+                    return Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} not enough operands", "Error:".red().bold()),
+                    ));
+                }
+                match stack.pop().unwrap() {
+                    StackElement::Integer(x) => match stack.pop().unwrap() {
+                        StackElement::Integer(y) => {
+                            stack.push(StackElement::Integer(x * y));
+                            Ok(())
+                        }
+                        _ => Err(Error::new(
+                            io::ErrorKind::InvalidInput,
+                            format!("{} I can only multiply Integers", "Error:".red().bold()),
+                        )),
+                    },
+                    _ => Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} I can only multiply Integers", "Error:".red().bold()),
+                    )),
+                }
+            }
+            "/" => {
+                if stack.len() < 2 {
+                    return Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} not enough operands", "Error:".red().bold()),
+                    ));
+                }
+                match stack.pop().unwrap() {
+                    StackElement::Integer(x) => match stack.pop().unwrap() {
+                        StackElement::Integer(y) => {
+                            stack.push(StackElement::Integer(x / y));
+                            Ok(())
+                        }
+                        _ => Err(Error::new(
+                            io::ErrorKind::InvalidInput,
+                            format!("{} I can only divide Integers", "Error:".red().bold()),
+                        )),
+                    },
+                    _ => Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} I can only divide Integers", "Error:".red().bold()),
+                    )),
+                }
+            }
+            "clear" => {
+                stack.clear();
+                Ok(())
+            }
+            "dup" => {
+                if stack.is_empty() {
+                    return Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} not enough operands", "Error:".red().bold()),
+                    ));
+                }
+                let a = stack.pop().unwrap();
+                stack.push(a.clone());
+                stack.push(a);
+                Ok(())
+            }
+            "swap" => {
+                if stack.len() < 2 {
+                    return Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} not enough operands", "Error:".red().bold()),
+                    ));
+                }
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(a.clone());
+                stack.push(b.clone());
+
+                Ok(())
+            }
+            "drop" => {
+                if stack.is_empty() {
+                    return Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} not enough operands", "Error:".red().bold()),
+                    ));
+                }
+                stack.pop().unwrap();
+
+                Ok(())
+            }
+            "rot" => {
+                if stack.len() < 3 {
+                    return Err(Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("{} not enough operands", "Error:".red().bold()),
+                    ));
+                }
+
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                let c = stack.pop().unwrap();
+
+                stack.push(b);
+                stack.push(a);
+                stack.push(c);
+
+                Ok(())
+            }
+            "call" => {
+                if let StackElement::SubStack(s) = stack.pop().unwrap() {
+                    return s.iter().try_for_each(|i| call(stack, i));
+                }
+
+                Ok(())
+            }
+            s => {
+                stack.push(StackElement::Word(s.to_string()));
+                Ok(())
+            }
+        },
+        StackElement::Integer(n) => {
+            stack.push(StackElement::Integer(*n));
+            Ok(())
         }
-        StackElement::Integer(n) => stack.push(StackElement::Integer(*n)),
-        StackElement::SubStack(s) => stack.push(StackElement::SubStack(s.clone())),
+        StackElement::SubStack(s) => {
+            stack.push(StackElement::SubStack(s.clone()));
+            Ok(())
+        }
+        StackElement::Word(s) => {
+            stack.push(StackElement::Word(s.clone()));
+            Ok(())
+        }
     }
-    
 }
